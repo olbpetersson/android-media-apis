b55f2fc2e65954654f61e09f4eb3b7c802db08c1
Ola Petersson <olbpetersson@gmail.com>
2019-12-27 00:43:33 +0100

    saving some more documentation

--- app/src/main/java/se/olapetersson/test/ui/main/MainViewModel.kt
+++ app/src/main/java/se/olapetersson/test/ui/main/MainViewModel.kt
@@ -12,20 +12,22 @@ import java.nio.ByteBuffer
 class MainViewModel : ViewModel() {
 
     // most of this is from https://stackoverflow.com/questions/35379000/extract-audio-from-mp4-and-save-to-sd-card-mediaextractor
+    // Might be able to use to analyze sound data: https://newventuresoftware.com/blog/record-play-and-visualize-raw-audio-data-in-android.html
+    // https://github.com/newventuresoftware/WaveformControl/blob/master/app/src/main/java/com/newventuresoftware/waveformdemo/MainActivity.java
     init {
-        "initting in viewmodel".debug()
+        debug("initting in viewmodel")
         val mediaExtractor = MediaExtractor()
         mediaExtractor.setDataSource("/sdcard/Download/test.mp4")
         val trackCount = mediaExtractor.trackCount
-        "This is the trackCount: $trackCount".debug()
-        val muxer = MediaMuxer("/sdcard/Download/output", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
-        (0 until mediaExtractor.trackCount).forEach {
-            "$it".debug()
-            val format = mediaExtractor.getTrackFormat(it)
-            "this is the format $format".debug()
+        debug("This is the trackCount: $trackCount")
+        val muxer = MediaMuxer("/sdcard/Download/output.mp4", MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+        //(0 until mediaExtractor.trackCount).forEach {
+            val format = mediaExtractor.getTrackFormat(1)
+            debug("this is the format $format")
             // index 1 seems to be the audio
-            val audioTrack = mediaExtractor.selectTrack(1)
-            "this is the audioTrack $audioTrack".debug()
+            val audioTrack = 1
+            mediaExtractor.selectTrack(audioTrack)
+            muxer.addTrack(format)
             var bufferSize = 0
             if (format.containsKey(MediaFormat.KEY_MAX_INPUT_SIZE)) {
                 val newSize = format.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
@@ -39,29 +41,35 @@ class MainViewModel : ViewModel() {
                 bufferInfo.offset = 0;
                 bufferInfo.size = mediaExtractor.readSampleData(audioByteBuffer, 0)
                 if (bufferInfo.size < 0) {
-                    "Saw input EOS.".debug()
+                    debug("Saw input EOS.")
+                    debug("this is the audioByteBuffer $audioByteBuffer")
+                    audioByteBuffer.rewind()
+                    debug("audi bytebuffer has remaingin: ${audioByteBuffer.hasRemaining()}")
+                    while(audioByteBuffer.hasRemaining()) {
+                        debug("bajtbuffer: ${audioByteBuffer.position()} : ${audioByteBuffer.get()}")
+                    }
                     bufferInfo.size = 0;
                     break;
                 } else {
                     bufferInfo.presentationTimeUs = mediaExtractor.getSampleTime()
                     if (endMs > 0 && bufferInfo.presentationTimeUs > (endMs * 1000)) {
-                        "The current sample is over the trim end time.".debug()
+                        debug("The current sample is over the trim end time.")
                         break;
                     } else {
-                        bufferInfo.flags = mediaExtractor.getSampleFlags()
-                        val trackIndex = mediaExtractor.getSampleTrackIndex()
-                        muxer.writeSampleData(1, audioByteBuffer, bufferInfo)
+                        bufferInfo.flags = mediaExtractor.sampleFlags
+                        val trackIndex = mediaExtractor.sampleTrackIndex
+                        muxer.writeSampleData(0, audioByteBuffer, bufferInfo)
                         mediaExtractor.advance()
                     }
                 }
             }
             muxer.stop()
             muxer.release()
-        }
+        //}
     }
 }
 
 
-fun String.debug() {
-    Log.i("DEBUG", this)
+inline fun debug(string: String) {
+    Log.i("DEBUG", string)
 }
